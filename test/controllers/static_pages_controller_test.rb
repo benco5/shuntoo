40adde2e8772376e4af23ae53f66abcf89e295ab
Benco <bcos34@gmail.com>
2014-10-21 18:41:19 -0400

    Work on html/css (navbar, home welcome unit)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Shuntoo"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Shuntoo"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Shuntoo"
+
   end
 
 end
